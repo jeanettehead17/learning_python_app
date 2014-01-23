92390b18f22ce84402f56796d68e41f0c6f69b10
Sarah Guido <sarguido@umich.edu>
2014-01-23 01:06:10 -0500

    More work on 2, fixed 1, added 3, added scripts.

---
+++ scripts/script2.py
@@ -0,0 +1,15 @@
+# print 'Hello, world!'
+
+x = 5
+
+# If statement
+if x == 5:
+    print 'x is equal to 5'
+
+y = 10
+
+# If/else statement
+if x > y:
+    print 'x is greater than y'
+else:
+    print 'y is greater than x'
---
+++ scripts/script3.py
@@ -0,0 +1,10 @@
+x = 5                                                                                                         y = 10
+z = 15
+
+# If/else statement
+
+# Checking if y is both greater than x and less than z
+if x < y and y < z:                                                                                               print 'y is in the middle'
+# Checking if z is less than y or less than x 
+elif z < y or z < x:
+    print 'z is less'
